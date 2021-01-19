156fccdb5cb3a66165806c9fff6d9590bb76aac8
Sam Rodriguez <rodriguez_414@yahoo.com>
2021-01-18 21:48:32 -0800

    added api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,76 @@
+const db = require("../models");
+
+module.exports = (app) => {
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).sort({ _id: -1 }).limit(1)
+            .then((dbWorkout) => db.Workout.aggregate([
+                {
+                    $addFields: {
+                        totalDuration: { $sum: "$exercises.duration" }
+                    }
+                }
+            ])
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            }));
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.updateOne({ 
+            _id: req.params.id 
+        }, 
+        { 
+            $push: {
+                exercises: req.body
+            }
+        })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.aggregate([
+            {
+                $sort: { _id: -1 }
+            },
+            {
+                $limit: 7
+            },
+            {
+                $addFields: {
+                    totalDuration: { $sum: "$exercises.duration" },
+                    totalPounds: { $sum: "$exercises.weight" }
+                }
+            },
+            {
+                $sort: { _id: 1 }
+            }
+        ])
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+};
--- server.js
+++ server.js
@@ -1,107 +1,24 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
-
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models/workout");
-
+const db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Tracker", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.get("/api/workouts", (req, res) => {
-    db.find({})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.json(err);
-    })
-})
-
-app.post("/api/workouts", (req, res) => {
-    db.create(req.body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.json(err);
-    })
-})
-
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/exercise.html'));
-})
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/stats.html'));
-})
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-})
-
-
-// db.User.create({ name: "Ernest Hemingway" })
-//   .then(dbUser => {
-//     console.log(dbUser);
-//   })
-//   .catch(({ message }) => {
-//     console.log(message);
-//   });
-
-// app.get("/notes", (req, res) => {
-//   db.Note.find({})
-//     .then(dbNote => {
-//       res.json(dbNote);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/user", (req, res) => {
-//   db.User.find({})
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.post("/submit", ({ body }, res) => {
-//   db.Note.create(body)
-//     .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/populateduser", (req, res) => {
-//   db.User.find({})
-//     .populate("notes")
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
