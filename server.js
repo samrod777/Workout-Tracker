c6dd72d3a8ea2668a86c12803f896c27838d9395
Sam Rodriguez <rodriguez_414@yahoo.com>
2021-01-23 15:13:35 -0800

    minor fixes for logic

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
